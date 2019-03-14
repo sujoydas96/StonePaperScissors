be0fd4fcd1ca01632cae64ecf712f24955a63464
Sujoy Das <sujoydas96>
2019-03-14 09:08:03 +0530

    Debugged some Errors. Need some more change in logic

--- diceML.cpp
+++ diceML.cpp
@@ -8,6 +8,10 @@ using namespace std;
 int ans[3][3]={{0,0,0},{0,0,0},{0,0,0}};
 int var=0;
 
+const string Rock="Rock";
+const string Paper="Paper";
+const string Scissors="Scissors";
+
 void check(string inp,string answer);
 string GenAnswer(int i);
 string result(string inp);
@@ -16,40 +20,38 @@ int main()
 {
     string inp;
     int a=0;
-    
-    do
-    {
-        system("CLS");
+   while(1)
+   {
+        //system("CLS");
         cout<<"\nYour Turn:(Rock, Paper or Scissors) "<<endl;
         getline(cin,inp);
         cout<<"\nComputer output: "<<result(inp)<<endl;
-        cout<<"press 1 to continue again\n"<<endl;
-        cin>>a;
-    }
-    while(a);
+        cout<<"\n\n\n";
+   }
+
 return 0;
-}    
+}
 
 string result(string inp)//return the answer after computing the highest element in the row
-{   
+{
     int max;
     int i=0;
     int index=0;
     string answer;
 
-    if(inp.compare("Rock")==0)
+    if(inp.compare(Rock)==0)
     {
         max=ans[0][i];
         for(i=0;i<3;i++)
         {
             if(max<ans[0][i])
             {
-               max=ans[0][i]; 
+               max=ans[0][i];
                index=i;
-            } 
+            }
         }
     }
-    else if(inp.compare("Paper")==0)
+    else if(inp.compare(Paper)==0)
     {
         max=ans[1][i];
         for(i=0;i<3;i++)
@@ -57,11 +59,11 @@ string result(string inp)//return the answer after computing the highest element
             if(max<ans[1][i])
             {
                max=ans[1][i];
-               index=i; 
-            } 
+               index=i;
+            }
         }
     }
-    else if(inp.compare("Scissors")==0)
+    else if(inp.compare(Scissors)==0)
     {
         max=ans[2][i];
         for(i=0;i<3;i++)
@@ -70,78 +72,90 @@ string result(string inp)//return the answer after computing the highest element
             {
                 max=ans[2][i];
                 index=i;
-            } 
+            }
         }
     }
-    else 
-    cout<<"Failed to Compute"<<endl;
+    else
+    {
+        cout<<"Failed to Compute"<<endl;
+    }
+
     answer=GenAnswer(index);
     if(var<=100)
     {
-        check(inp,answer);          
+        check(inp,answer);
     }
     return answer;
 }
 
-string GenAnswer(int index)
+string GenAnswer(int index)           //generates the answer for the function result().
 {
-    if(index==0)return "Rock";
-    else if(index==1)return "Paper";
-    else if(index==2)return "Scissors";
-    else return "Error... Failed to generate Answer\n"; 
-}  
-
+    if(index==0)return Rock;
+    else if(index==1)return Paper;
+    else if(index==2)return Scissors;
+    else return "Error... Failed to generate Answer\n";
+}
+//substraction important for better efficiency in the beginning
 void check(string inp,string answer)
 {
-    if(inp.compare("Rock")==0)
+    if(inp.compare(Rock)==0)
     {
-        if(answer.compare("Rock")==0)
+        if(answer.compare(Rock)==0)
         {
             ans[0][0]=ans[0][0]-1;
+            cout<<"\nComputer Loses\n"<<endl;
         }
-        else if(answer.compare("Paper")==0)
+        else if(answer.compare(Paper)==0)
         {
             ans[0][1]=ans[0][1]+1;
+            cout<<"\nComputer Wins\n"<<endl;
         }
-        else if(answer.compare("Scissors")==0)
+        else if(answer.compare(Scissors)==0)
         {
             ans[0][2]=ans[0][2]-1;
+            cout<<"\nComputer Loses\n"<<endl;
         }
     }
 
-    if(inp.compare("Paper")==0)
+    if(inp.compare(Paper)==0)
     {
-       
-        if(answer.compare("Rock")==0)
+
+        if(answer.compare(Rock)==0)
         {
-            ans[0][0]=ans[0][0]-1;
+            ans[1][0]=ans[1][0]-1;
+            cout<<"\nComputer Loses\n"<<endl;
         }
-        else if(answer.compare("Paper")==0)
+        else if(answer.compare(Paper)==0)
         {
-            ans[0][1]=ans[0][1]-1;
+            ans[1][1]=ans[1][1]-1;
+            cout<<"\nComputer Loses\n"<<endl;
         }
-        else if(answer.compare("Scissors")==0)
+        else if(answer.compare(Scissors)==0)
         {
-            ans[0][2]=ans[0][2]+1;
+            ans[1][2]=ans[1][2]+1;
+            cout<<"\nComputer Wins\n"<<endl;
         }
     }
 
-    if(inp.compare("Scissors")==0)
+    if(inp.compare(Scissors)==0)
     {
-       
-        if(answer.compare("Rock")==0)
+
+        if(answer.compare(Rock)==0)
         {
-            ans[0][0]=ans[0][0]+1;
+            ans[2][0]=ans[2][0]+1;
+            cout<<"\nComputer Wins\n"<<endl;
         }
-        else if(answer.compare("Paper")==0)
+        else if(answer.compare(Paper)==0)
         {
-            ans[0][1]=ans[0][1]-1;
+            ans[2][1]=ans[2][1]-1;
+            cout<<"\nComputer Loses\n"<<endl;
         }
-        else if(answer.compare("Scissors")==0)
+        else if(answer.compare(Scissors)==0)
         {
-            ans[0][2]=ans[0][2]-1;
-        } 
+            ans[2][2]=ans[2][2]-1;
+            cout<<"\nComputer Loses\n"<<endl;
+        }
     }
-    
-var++;    
+
+var++;
 }
